0c33d935407c756f4ef994d6f419a1b3c62b4878
amineZargouni <mohamedamine.zargouni@esprit.tn>
2021-03-19 19:56:32 +0100

    admin role

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -5,15 +5,15 @@ const router = express.Router();
 const { getProducts, newProduct, getSingleProduct, updateProduct, deleteProduct }
     = require('../controllers/productController');
 
-const {isAuthenticatedUser} = require('../middlewares/auth')
+const { isAuthenticatedUser,authorizedRoles } = require('../middlewares/auth')
 router.route('/products').get(getProducts);
 router.route('/product/:id').get(getSingleProduct);
 
 //admin
-router.route('/admin/product/new').post(isAuthenticatedUser,newProduct);
+router.route('/admin/product/new').post(isAuthenticatedUser,authorizedRoles('admin'), newProduct);
 router.route('/admin/product/:id')
-    .put(isAuthenticatedUser,updateProduct)
-    .delete(isAuthenticatedUser,deleteProduct);
+    .put(isAuthenticatedUser,authorizedRoles('admin'), updateProduct)
+    .delete(isAuthenticatedUser,authorizedRoles('admin'), deleteProduct);
 
 
 module.exports = router;
